dc6f586d50c421d579d083bce23ecdbe13b9e9c9
Ioannis Tsingalis <ioannis.tsingalis@gmail.com>
2024-05-20 22:34:29 +0300

    Update

--- splits/create_splits.py
+++ splits/create_splits.py
@@ -20,7 +20,6 @@ def main():
 
     dev_names = [filename for filename in os.listdir(args.input_dir) if
                  os.path.isdir(os.path.join(args.input_dir, filename))]
-
     # class_names = ['D01', 'D02', 'D03', 'D04', 'D05', 'D06', 'D07', 'D08', 'D09', 'D10', 'D11', 'D12', 'D13', 'D14',
     #                'D15', 'D16', 'D17', 'D18', 'D19', 'D20', 'D21', 'D22', 'D23', 'D24', 'D25', 'D26', 'D27', 'D28',
     #                'D29', 'D30', 'D31', 'D32', 'D33', 'D34', 'D35']
